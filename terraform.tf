9e677b3ceb3fc971419954c6ffaf04d9b8d3a585
maaadprod <38993598+maaadprod@users.noreply.github.com>
2023-10-28 01:06:52 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region = "eu-west-3"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 resource "aws_instance" "example" {
   instance_type = "t2.micro"
